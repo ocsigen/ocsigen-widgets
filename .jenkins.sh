79147e59bf7828780bb6ba0abad20f5a774f7228
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-09-05 19:07:52 +0200

    jenkins

---
+++ .jenkins.sh
@@ -0,0 +1,15 @@
+
+opam pin add --no-action ocsigen-widgets .
+opam install --deps-only ocsigen-widgets
+opam install --verbose ocsigen-widgets
+
+do_build_doc () {
+  make doc
+#  cp -Rf doc/manual/files/* ${MANUAL_FILES_DIR}/
+#  cp -Rf doc/manual/src/* ${MANUAL_SRC_DIR}/
+#  cp -Rf doc/api/wiki/*.wiki ${API_DIR}/
+}
+
+do_remove () {
+  opam remove --verbose js_of_ocaml
+}
