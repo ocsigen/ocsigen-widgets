f07a51271f98b063ce7dd62e9868e5219560c8e0
Vasilis Papavasileiou <git@vasilis.airpost.net>
2016-04-01 17:04:38 +0200

    Fix Jenkins, pass 'opam lint'

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,4 +1,3 @@
-opam pin add --no-action tyxml 'https://github.com/ocsigen/tyxml.git#master'
 opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
 #opam pin add --no-action deriving 'https://github.com/ocsigen/deriving.git#master'
 opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
